4bfffcd8b0947a768194fb8dcb3f7c8e99f753ce
Anete Vaalu <55079673+anetevlu@users.noreply.github.com>
2020-03-13 13:37:13 +0200

    update

--- w06_liides/tund_06/Proov3Kuvamine.java
+++ w06_liides/tund_06/Proov3Kuvamine.java
@@ -9,7 +9,7 @@ public class Proov3Kuvamine{
 		Arvutatav[] ad=new Arvutatav[5];
 		ad[0]=new RingiPindala();
 		ad[1]=new RistkylikuPindala(3);
-        ad[2]=new RistkylikuPindala(8);
+        	ad[2]=new RistkylikuPindala(8);
 		ad[3]=new RistkylikuPindala(5);
 		ad[4]=new RisttahukaRuumala(1,2);
 		for(int i=0; i<ad.length; i++){
@@ -17,3 +17,11 @@ public class Proov3Kuvamine{
 		}
 	}
 }
+
+/*
+3.141592653589793 12.566370614359172 28.274333882308138 50.26548245743669 78.53981633974483
+3.0 6.0 9.0 12.0 15.0
+8.0 16.0 24.0 32.0 40.0
+5.0 10.0 15.0 20.0 25.0
+2.0 4.0 6.0 8.0 10.0
+*/
