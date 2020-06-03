585c839c16cb7107d669124b6186740c31c2af57
Anete Vaalu <55079673+anetevlu@users.noreply.github.com>
2020-06-03 11:48:10 +0300

    Delete Punktmass.java

--- w08_alamklassid/tund_8/versioon2/Punktmass.java
+++
@@ -1,8 +0,0 @@
-class Punktmass extends AbstraktnePunktmass{
-    double kg, x;
-    public Punktmass(double kg, double x){
-        this.kg=kg; this.x=x;
-    }
-    public double massikeseX(){return x;}
-    public double kysiKg(){return kg;}
-}
